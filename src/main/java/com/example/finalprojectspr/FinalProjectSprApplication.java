4fb45e2d66dd346f55b3936dc9858c9e3edf3d95
WorkAndPlay <karlsirkas@outlook.com>
2023-03-22 20:25:34 +0200

    Final-Project-Spring

---
+++ src/main/java/com/example/finalprojectspr/FinalProjectSprApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectSprApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectSprApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalprojectspr/FinalProjectSprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectspr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectSprApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
