4afdb86fa852a1403d005c8e24fcfcc1917bc93a
WorkAndPlay <karlsirkas@outlook.com>
2023-03-22 20:55:56 +0200

    Final-Project-Spring Update!

---
+++ src/main/java/com/example/finalprojectspr/models/Revenue.java
@@ -0,0 +1,18 @@
+package com.example.finalprojectspr.models;
+
+/**
+ * @author Karl-Erik Sirkas
+ * @ Date 22.03.2023
+ */
+public class Revenue {
+
+    public int sumOfAmountsForCarRental;
+
+    public int getSumOfAmountsForCarRental() {
+        return sumOfAmountsForCarRental;
+    }
+
+    public void setSumOfAmountsForCarRental(int sumOfAmountsForCarRental) {
+        this.sumOfAmountsForCarRental = sumOfAmountsForCarRental;
+    }
+}
---
+++ src/main/java/com/example/finalprojectspr/services/Customer.java
@@ -0,0 +1,4 @@
+package com.example.finalprojectspr.services;
+
+public interface Customer {
+}
